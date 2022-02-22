a614d5b93701b95f6d0b82eaa146ba92b31f973f
Nathan145 <quangnhan145@gmail.com>
2022-02-22 14:11:33 +0700

    Add new app

--- tiki/settings.py
+++ tiki/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'human',
+    'products',
+    'apartment',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +127,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = BASE_DIR / 'static'
+
+LOGIN_REDIRECT_URL = 'product_list'
+LOGOUT_REDIRECT_URL = 'login'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
